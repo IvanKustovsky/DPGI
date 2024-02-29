8dbc141c0fb6d15f1b211255917cad9f2a470542
IvanKustovsky <vanyakustovsky@gmail.com>
2024-02-29 19:09:12 +0200

    Add XML-docs comments

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+/// <summary>
+/// Основний клас програми.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Основний метод програми. Виконує вивід привітання у консоль.
+    /// </summary>
+    /// <param name="args">Масив аргументів командного рядка.</param>
+    static void Main(string[] args)
+    {
+        // Виводимо привітання в консоль
+        Console.WriteLine("Hello, World!");
+    }
+}
